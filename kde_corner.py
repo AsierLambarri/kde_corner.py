7fdb3cf63379debd34db7259f7dd236e9c496f4e
BR <109964584+AsierLambarri@users.noreply.github.com>
2024-11-05 11:40:42 +0100

    Create kde_corner.py

---
+++ kde_corner.py
@@ -0,0 +1,103 @@
+import arviz
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from copy import copy, deepcopy
+
+
+def kde_corner(arviz_data,
+               var_names,
+               arviz_kwargs = None,
+               marginals=True,
+               points_estimate="mean",
+               kind="kde",
+               colormap=cm.inferno,
+               figsize=(12,12),
+               save=None
+              ):
+
+    arfit = copy(arviz_data)
+
+    arviz_kwargs_default = {'kde_kwargs' : { 'hdi_probs': [0.25, 0.5, 0.75, 0.95],
+                                             'contourf_kwargs': { 'cmap': colormap,  
+                                                                },
+                                             'contour_kwargs': { 'levels': 6,  
+                                                                 'colors': ['none'] * 6 ,  
+                                                                 'zorder': 9
+                                                               },
+                                             'fill_last': False  
+                                             },
+                            
+                            'marginal_kwargs' : {'plot_kwargs': {'color': 'black'}
+                                                },
+                            
+                            'scatter_kwargs': {'s': 1, 'alpha': 1, 'zorder': -1
+                                              },
+                            'hexbin_kwargs': {'mincnt' : len(arfit.posterior[var_names[0]].values.flatten()) / len(arfit.posterior[var_names[0]]) * 0.02,
+                                              'cmap' : colormap
+                                             }
+                            }
+                 
+    
+    updated_kwargs = deepcopy(arviz_kwargs_default)
+    if arviz_kwargs is not None:
+        for key, value in arviz_kwargs.items():
+            if key in updated_kwargs and isinstance(value, dict):
+                for nested_key, nested_value in value.items():
+                    updated_kwargs[key][nested_key] = nested_value
+            else:
+                updated_kwargs[key] = value
+    else:
+        pass
+
+    updated_kwargs['kde_kwargs'].update({'contourf_kwargs': { 'cmap': colormap,  
+                                                            }
+                                        })
+    
+    updated_kwargs['kde_kwargs'].update({'hexbin_kwargs': { 'cmap': colormap,  
+                                                            }
+                                        })
+    print(updated_kwargs)
+    fig, axes = plt.subplots(nrows=len(var_names), ncols=len(var_names), figsize=figsize)
+    
+    arviz.plot_pair(
+        arfit,
+        var_names=var_names,
+        marginals=marginals,
+        point_estimate=points_estimate,
+        kind=kind,
+        ax=axes,
+        **updated_kwargs,
+        show=False
+    )
+    
+    summary = arviz.summary(arfit)
+    
+    xlabels = [axes[-1,j].get_xlabel() for j in range(4)]
+    ylabels = [axes[i, 0].get_ylabel() for i in range(4)]
+    
+    for j in range(len(axes)):
+        pf = "+" + str(round(summary.loc[xlabels[j]]['hdi_97%'] - summary.loc[xlabels[j]][points_estimate],2))
+        pn = str(round(summary.loc[xlabels[j]]['hdi_3%'] - summary.loc[xlabels[j]][points_estimate], 2))
+        
+        axes[j, j].set_title(f"{xlabels[j]}={summary.loc[xlabels[j]]['mean']:.2f}$_{{{pn}}}^{{{pf}}}$")
+        
+    
+    
+    for i in range(len(axes)):
+        for j in range(len(axes)):
+            if i != j:
+                axes[i, j].scatter(
+                    arfit.posterior[xlabels[j]].values.flatten(),  
+                    arfit.posterior[ylabels[i]].values.flatten(),  
+                    s=updated_kwargs['scatter_kwargs']['s'],  # Marker size
+                    color=updated_kwargs['kde_kwargs']['contourf_kwargs']['cmap'](0),  
+                    alpha=updated_kwargs['scatter_kwargs']['alpha'],
+                    zorder=updated_kwargs['scatter_kwargs']['zorder']
+                )
+    
+    
+    plt.tight_layout()
+    if save is not None:
+        plt.savefig(save)
+    plt.close()
+    return fig
